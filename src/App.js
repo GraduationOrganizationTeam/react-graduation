bd3176fed07292bd6561193920f20aea5b72b449
Felipe Azank Santos <felipeazank@usp.br>
2021-12-13 21:46:26 -0300

    add heroku integration

--- src/App.js
+++ src/App.js
@@ -10,22 +10,22 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    fetch('http://127.0.0.1:8000/api/alunos/')
+    fetch('https://projeto-graduation.herokuapp.com/api/alunos/')
       .then((response) => response.json())
       .then((result) => this.setState({ list_alunos: result }))
       .catch((error) => error);
     
-    fetch('http://127.0.0.1:8000/api/avaliacoes/')
+    fetch('https://projeto-graduation.herokuapp.com/api/avaliacoes/')
       .then((response) => response.json())
       .then((result) => this.setState({ list_eval: result }))
       .catch((error) => error);
 
-    fetch('http://127.0.0.1:8000/api/disciplinas/')
+    fetch('https://projeto-graduation.herokuapp.com/api/disciplinas/')
       .then((response) => response.json())
       .then((result) => this.setState({ list_disciplinas: result }))
       .catch((error) => error);
 
-    fetch('http://127.0.0.1:8000/api/comentario/')
+    fetch('https://projeto-graduation.herokuapp.com/api/comentario/')
       .then((response) => response.json())
       .then((result) => this.setState({ list_comentarios: result }))
       .catch((error) => error);
